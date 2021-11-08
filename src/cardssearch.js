524a02869966243ff8081bfc2bb9161ff777d0be
roselew <rozalewinska@gmail.com>
2021-11-08 10:17:44 +0100

    Cards Searching Script

---
+++ src/cardssearch.js
@@ -0,0 +1,68 @@
+//SYSTEM FLOW Cards Searching
+(() => {
+    class CardsSearch {
+        constructor(elem) {
+            this.searchInput = elem;
+            this.container = this.searchInput.closest(".container");
+            this.cards = this.container.querySelectorAll(".card");
+            this.textSelectors = "h1, h2, h3, h4, h5, h6, p";
+            this.assignEvents();
+        }
+
+        assignEvents() {
+            this.searchInput.addEventListener("keyup", this.searchResults.bind(this));
+        }
+
+        searchResults() {
+            this.queryText = this.searchInput.value.toLowerCase();
+
+            if (this.queryText.length > 0) {
+                this.cards.forEach((card) => {
+                    const fullText = card.textContent;
+                    if (fullText.toLowerCase().indexOf(this.queryText) > -1) {
+                        card.style.display = "flex";
+                        this.showResult(card);
+                    } else {
+                        card.style.display = "none";
+                    }
+                });
+            } else {
+                this.clearSearch();
+            }
+        }
+
+        showResult(card) {
+            const texts = card.querySelectorAll(this.textSelectors);
+            texts.forEach((text) => {
+                text.innerHTML = this.highlightText(text.innerText);
+            });
+        }
+
+        hideResult(card) {
+            const texts = card.querySelectorAll(this.textSelectors);
+            texts.forEach((text) => {
+                text.innerHTML = text.innerText;
+            });
+        }
+
+        clearSearch() {
+            this.cards.forEach((card) => {
+                card.style.display = "flex";
+                this.hideResult(card);
+            });
+        }
+
+        highlightText(text) {
+            const myReg = new RegExp(`(${this.queryText})`, "gi");
+            return text.replace(myReg, "<span style='background: yellow'>$1</span>");
+        }
+    }
+
+    const searches = document.querySelectorAll("[data-sysflow-cards-search");
+
+    if (searches) {
+        searches.forEach((search) => {
+            new CardsSearch(search);
+        });
+    }
+})();
