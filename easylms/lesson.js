23fa8c473b25b502024bcd9301cee78975a50b56
roselew <rozalewinska@gmail.com>
2022-02-01 12:48:01 +0100

    AutoplayMode Test

--- easylms/lesson.js
+++ easylms/lesson.js
@@ -38,6 +38,7 @@ class Lesson {
         );
     }
     checkIfAutoplay() {
+        console.log(this.autoplayMode);
         if (this.autoplayMode) {
             this.autoplayBtn.click();
         }
