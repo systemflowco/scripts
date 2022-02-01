33e9bb866d6e40cdd5d7f54fefe5fed53fba504d
roselew <rozalewinska@gmail.com>
2022-02-01 11:07:24 +0100

    EasyLms Scripts

---
+++ easylms/continuje.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    const lastLesson = JSON.parse(localStorage.getItem("LESSON.LAST"));
+    const continueBanner = document.querySelector(".continue-banner");
+    if (lastLesson) {
+        continueBanner.href = `./lekcje/${lastLesson.lessonSlug}`;
+        continueBanner.querySelector(".continue-course-name-span").innerText = lastLesson.course;
+        continueBanner.querySelector(".continue-lesson-name-span").innerText = lastLesson.lesson;
+    } else {
+        continueBanner.style.display = "none";
+    }
+});
---
+++ easylms/lesson.js
@@ -0,0 +1,68 @@
+class Lesson {
+    constructor(elem) {
+        this.pathname = location.pathname;
+        this.slug = this.pathname.match(/(?<=lekcje\/).+/)[0];
+        this.finishBtn = document.querySelector("[data-lms-finish-btn]");
+        this.finishedBtn = document.querySelector("[data-lms-finished-btn]");
+        this.KEY_FINISH = "LESSON.FINISH";
+        this.KEY_LAST = "LESSON.LAST";
+        this.finishedLessons = JSON.parse(localStorage.getItem(this.KEY_FINISH)) || [];
+        this.lessons = document.querySelectorAll(".course-content-lesson");
+        this.lessonTitle = document.querySelector(
+            ".course-content-lesson.w--current .lesson-name"
+        ).innerText;
+        this.courseTitle = document.querySelector(".course-title-progress").innerText;
+        this.assignEvents();
+    }
+    assignEvents() {
+        localStorage.setItem(
+            this.KEY_LAST,
+            JSON.stringify({
+                lesson: this.lessonTitle,
+                lessonSlug: this.slug,
+                course: this.courseTitle,
+            })
+        );
+        this.checkIfFinished();
+        this.checkAllLessons();
+        this.finishBtn.addEventListener("click", this.finishLesson.bind(this));
+    }
+    checkIfFinished() {
+        if (this.finishedLessons.indexOf(this.slug) > -1) {
+            this.toggleFinishStatus();
+        }
+    }
+    checkAllLessons() {
+        let nOfFinished = 0;
+        this.lessons.forEach((lesson) => {
+            lesson.slug = lesson.href.match(/(?<=lekcje\/).+/)[0];
+            if (this.finishedLessons.indexOf(lesson.slug) > -1) {
+                lesson.querySelector(".not-done").style.display = "none";
+                lesson.querySelector(".is-done").style.display = "block";
+                nOfFinished++;
+            }
+        });
+        const progress = Math.round(100 * (nOfFinished / this.lessons.length));
+        document.querySelector(".course-progress-circle-text").innerText = `${progress}%`;
+        const circle = document.querySelector(".progress-ring__circle");
+        const radius = circle.r.baseVal.value;
+        const circumference = radius * 2 * Math.PI;
+        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+        circle.style.strokeDashoffset = circumference;
+        const offset = circumference - (progress / 100) * circumference;
+        circle.style.strokeDashoffset = offset;
+    }
+    toggleFinishStatus() {
+        this.finishBtn.style.display = "none";
+        this.finishedBtn.style.display = "flex";
+    }
+    finishLesson() {
+        this.toggleFinishStatus();
+        this.finishedLessons.push(this.slug);
+        this.checkAllLessons();
+        localStorage.setItem(this.KEY_FINISH, JSON.stringify(this.finishedLessons));
+    }
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+    new Lesson();
+});
