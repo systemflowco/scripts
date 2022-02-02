961394c745403093817ccdb25f4d69fdfd3ae347
roselew <rozalewinska@gmail.com>
2022-02-02 13:16:45 +0100

    Vimeo Onload

--- easylms/vimeo.js
+++ easylms/vimeo.js
@@ -1,16 +1,20 @@
 // inject the vimeo api script
-$('<script src="https://player.vimeo.com/api/player.js">').insertBefore($("script")[0]);
+$('<script onload="vimeoReady();"; src="https://player.vimeo.com/api/player.js">').insertBefore(
+    $("script")[0]
+);
 
-$("iframe").each((i, frame) => {
-    // for each iframe
-    // get the src
-    let src = $(frame).attr("src");
-    // skip the iframe if it's not a vimeo video
-    if (!src.includes("vimeo")) return;
+function vimeoReady() {
+    $("iframe").each((i, frame) => {
+        // for each iframe
+        // get the src
+        let src = $(frame).attr("src");
+        // skip the iframe if it's not a vimeo video
+        if (!src.includes("vimeo")) return;
 
-    // call the createPlayer function with the iframe's id
-    createPlayer(frame);
-});
+        // call the createPlayer function with the iframe's id
+        createPlayer(frame);
+    });
+}
 
 // setup the createPlayer function
 function createPlayer(iframe) {
