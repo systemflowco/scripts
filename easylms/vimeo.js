234cbc68b4014871e4ad1052133cb33c2c72ff82
roselew <rozalewinska@gmail.com>
2022-02-02 13:01:20 +0100

    Create Vimeo Player

--- easylms/vimeo.js
+++ easylms/vimeo.js
@@ -1,2 +1,23 @@
 // inject the vimeo api script
 $('<script src="https://player.vimeo.com/api/player.js">').insertBefore($("script")[0]);
+
+$("iframe").each((i, frame) => {
+    // for each iframe
+    // get the src
+    let src = $(frame).attr("src");
+    // skip the iframe if it's not a vimeo video
+    if (!src.includes("vimeo")) return;
+
+    // call the createPlayer function with the iframe's id
+    createPlayer(frame);
+});
+
+// setup the createPlayer function
+function createPlayer(iframe) {
+    var player = new Vimeo.Player(iframe);
+    console.log(player);
+
+    player.on("ended", function () {
+        console.log("the end");
+    });
+}
