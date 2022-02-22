52a73378b727bb4127b7ee4ec88e65ad7b07bc13
roselew <rozalewinska@gmail.com>
2022-02-22 10:36:02 +0100

    Vimeo Test3

--- easylms/vimeo.js
+++ easylms/vimeo.js
@@ -24,9 +24,9 @@
                 return;
             }
             // get the video id from the webflow defined src attribute value
-            const videoId = src.split("vimeo.com%2Fvideo%2F")[1].split("%3F")[0];
-            // create a new src & embed
-            src = `${src}?embedded=true`;
+            const videoId = src.split("vimeo.com%2Fvideo%2F")[1].split("%26")[0];
+            // create a new src & embed the enablejsapi=1 query string
+            src = `https://vimeo.com/${videoId}?embedded=true`;
             // set the recreated src as the iframe's src
             $(frame).attr("src", src);
             // create a unique id for the iframe
