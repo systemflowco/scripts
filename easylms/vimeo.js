650d80f9a7bec263000279296e243c9191d1bba7
roselew <rozalewinska@gmail.com>
2022-02-25 11:36:07 +0100

    Fix Vimeo Script

--- easylms/vimeo.js
+++ easylms/vimeo.js
@@ -24,9 +24,10 @@
                 return;
             }
             // get the video id from the webflow defined src attribute value
-            const videoId = src.split("vimeo.com%2Fvideo%2F")[1].split("%3F")[0];
-            let tempHash = src.split("%3Fh%3D")[1];
-            const videoHash = tempHash ? tempHash.split("%26")[0] : "";
+            let tempId = src.split("vimeo.com%2Fvideo%2F");
+            const videoId = tempId.length > 1 ? tempId[1].split("%3F")[0] : "";
+            let tempHash = src.split("%3Fh%3D");
+            const videoHash = tempHash.length > 1 ? tempHash[1].split("%26")[0] : "";
             // create a new src & embed the enablejsapi=1 query string
             src = `https://player.vimeo.com/video/${videoId}${videoHash ? "?h=" + videoHash : ""}`;
             // set the recreated src as the iframe's src
