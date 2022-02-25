f3be798e26de18333a790c408ae4b7216bff9b0e
roselew <rozalewinska@gmail.com>
2022-02-25 23:30:34 +0100

    Webhook Url

--- easylms/visit.js
+++ easylms/visit.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    function sendWebhook(data) {
+    function sendWebhook(url, data) {
         const xhttp = new XMLHttpRequest();
-        xhttp.open("POST", this.webhookUrl, true);
+        xhttp.open("POST", url, true);
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send(JSON.stringify(data));
     }
 
     const webhookUrlField = document.querySelector("[data-easylms-webhook-url]");
     const webhookUrl = webhookUrlField ? webhookUrlField.innerText : "";
-    console.log(webhookUrl);
+
     if (webhookUrl && webhookUrl !== "https://webhook_url.com") {
-        sendWebhook({
+        sendWebhook(webhookUrl, {
             type: "visitPage",
             location: window.location.href,
             ecUserId: window._EC_USER_ID,
