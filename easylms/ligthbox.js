ec218b684c815fe66020feb0824a0edf01cdacd8
roselew <rozalewinska@gmail.com>
2022-03-01 11:28:09 +0100

    LigthBox2

--- easylms/ligthbox.js
+++ easylms/ligthbox.js
@@ -9,11 +9,26 @@ function replaceVideoLink(link) {
 }
 
 function replaceYoutubeLink(link) {
-    return link;
+    var myregexp =
+        /.*(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi;
+    const videoId = link.replace(myregexp, "$1");
+
+    // create a new src & embed the enablejsapi=1 query string
+    const src = `https://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=${location.origin}&modestbranding=1&showinfo=0&rel=0`;
+
+    return src;
 }
 
 function replaceVimeoLink(link) {
-    return link;
+    var myregexp =
+        /.*(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^\/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_\-]+)?\/?(.*)/i;
+
+    const videoId = videoLink2.replace(myregexp, "$1");
+    const videoHash = videoLink2.replace(myregexp, "$2");
+    // create a new src & embed the enablejsapi=1 query string
+    const src = `https://player.vimeo.com/video/${videoId}${videoHash ? "?h=" + videoHash : ""}`;
+    // set the recreated src as the iframe's src
+    return src;
 }
 
 var Webflow = Webflow || [];
