6c68639c7ac379bbfaf9dbbd37112864eb587bda
roselew <rozalewinska@gmail.com>
2022-03-28 16:53:22 +0200

    Autoplay Info

--- easylms/lesson_easytools.js
+++ easylms/lesson_easytools.js
@@ -27,6 +27,7 @@
             this.assignEvents();
         }
         assignEvents() {
+            this.showAutoPlayInfo();
             this.saveLastLesson();
 
             //if no easyJson Script do not wait for data
@@ -212,6 +213,27 @@
                 xhttp.open("POST", this.webhookUrl, true);
                 xhttp.setRequestHeader("Content-type", "application/json");
                 xhttp.send(JSON.stringify(data));
+            }
+        }
+        showAutoPlayInfo() {
+            const autoPlayTooltip = document.createElement("div");
+            autoPlayTooltip.innerHTML = `<div class="tooltip-trigger" data-ix="tooltip-hover" style="display: inline-block;"><div class="tooltip-trigger-content"><div class="tooltip" style="display: none;"><div class="text-block c-t-neutral-10">Część przeglądarek, np. Safari, blokuje autoodtwarzanie. <br>Jeśli ta opcja nie działa poprawnie w Twojej przeglądarce, <a href="https://www.notion.so/Autoodtwarzanie-8a148c1cbb6c4091b67127554a3b8475">zobacz jak ją odblokować</a></div><div class="tooltip-arrow"></div></div><div class="material-icons">info</div></div></div>`;
+            autoPlayTooltip.style.paddingLeft = "4px";
+            const tooltip = autoPlayTooltip.querySelector(".tooltip");
+            tooltip.style.top = "28px";
+            tooltip.style.bottom = "auto";
+            const arrow = autoPlayTooltip.querySelector(".tooltip-arrow");
+            arrow.style.top = "-6px";
+            arrow.style.bottom = "auto";
+            const autoPlayBtnLabel = document.querySelector("[data-lms-autoplay-btn]");
+            if (autoPlayBtnLabel) {
+                autoPlayBtnLabel.append(autoPlayTooltip);
+                autoPlayTooltip.addEventListener("mouseenter", () => {
+                    autoPlayTooltip.querySelector(".tooltip").style.display = "block";
+                });
+                autoPlayTooltip.addEventListener("mouseleave", () => {
+                    autoPlayTooltip.querySelector(".tooltip").style.display = "none";
+                });
             }
         }
     }
