1804a59e825297ff33795cf6bc6a68d7102638f4
roselew <rozalewinska@gmail.com>
2022-03-29 12:28:04 +0200

    Wistia Test

--- easylms/wistia.js
+++ easylms/wistia.js
@@ -14,13 +14,20 @@
 function wistiaReady() {
     const frame = document.querySelector("[data-easylms-video]");
 
+    // get the video id from the webflow defined src attribute value
+    var myregexp = /.*wistia.com(?:\/medias)?\/(.*)/i;
+    const videoId = window.easyLmsInfo.lessonVideo.replace(myregexp, "$1");
+
     // create a new src & embed the enablejsapi=1 query string
-    const src = window.easyLmsInfo.lessonVideo.replace("/medias/", "/embed/iframe/");
+    const src = `https://fast.wistia.net/embed/iframe/embed/iframe/${videoId}`;
 
     // set the recreated src as the iframe's src
     frame.setAttribute("src", src);
     // create a unique id for the iframe
     frame.id = "wistiaPlayer";
+    frame.setAttribute("allow", "autoplay");
+    frame.classList.add("wistia_embed");
+    frame.setAttribute("name", "wistia_embed");
     // call the createPlayer function with the iframe's id
     createWistiaPlayer(frame.id);
 }
@@ -57,4 +64,11 @@ function createWistiaPlayer(iframe) {
     // $(document).on("setPlaybackRate", (event, playrate) => {
     //     player.playbackRate(playrate);
     // });
+}
+
+{
+    /* <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
+    <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
+        <iframe src="https://fast.wistia.net/embed/iframe/3fml1o1h3e?videoFoam=true" title=" [Example Video] Wistia Video Essentials" allow="autoplay; fullscreen" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" msallowfullscreen width="100%" height="100%"></iframe></div></div>
+<script src="https://fast.wistia.net/assets/external/E-v1.js" async></script> */
 }
