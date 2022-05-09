dd4c1e7e9c98af57c41227b8c9667282e43e6769
roselew <rozalewinska@gmail.com>
2022-05-09 15:11:55 +0200

    Add Carousel Script

--- src/carousel.js
+++ src/carousel.js
@@ -8,6 +8,32 @@
 
         assignEvents() {
             console.log(this.cards);
+
+            const sliderLink = document.createElement("script");
+            sliderLink.src =
+                "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js";
+            sliderLink.id = "tiny-slider-script";
+
+            document.head.appendChild(sliderLink);
+
+            sliderLink.onload = ()=>{
+                loveSlider=tns({
+                    container:this.cards,
+                    items:1,
+                    slideBy:"page",
+                    gutter:10,
+                    nextButton:document.querySelector(".slider-arrow-right"),
+                    prevButton:document.querySelector(".slider-arrow-left"),
+                    loop:true,
+                    autoplay:true,
+                    navPosition:"bottom",
+                    responsive:{640:{items:2},1020:{items:3}}
+                })
+            }
+          
+
+
+
         }
 
      
