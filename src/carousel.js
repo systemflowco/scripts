37dfa85a44f8baa4d55da20f4dbf92797cf30a66
roselew <rozalewinska@gmail.com>
2022-05-10 22:02:26 +0200

    Minified Carousel Script

--- src/carousel.js
+++ src/carousel.js
@@ -2,11 +2,12 @@
     class Carousels {
         constructor(elem) {
             this.cards = elem;
+            this.animationSpeed = +elem.dataset.sysflowCarousel;
             this.container = $(">div", this.cards);
             this.slides = $(">div>div", this.cards);
             this.firstSlide = $(">div>div:first-of-type", this.cards);
             this.slidesNumber = this.slides.length;
-            this.speed = this.slidesNumber * 2;
+            this.speed = this.slidesNumber * this.animationSpeed;
             this.animationStyle = document.createElement("style");
             this.carouselOn = false;
             this.assignEvents();
@@ -16,25 +17,27 @@
             // Please run it with window.onload, not with document.ready
             this.checkSmoothScrolling();
             $(window).on("resize", this.checkSmoothScrolling.bind(this));
+            $(this.cards).on("mouseenter", this.stopScrolling.bind(this));
+            $(this.cards).on("mouseleave", this.checkSmoothScrolling.bind(this));
         }
 
         checkSmoothScrolling() {
-           
             // detect number of visible slides
             var slidesVisible = $(this.cards).width() / this.firstSlide.outerWidth(false);
             slidesVisible = Math.ceil(slidesVisible);
 
+            console.log(slidesVisible);
+            if (this.carouselOn) {
+                this.clearScrolling();
+            }
+
             // count slides to determine animation speed
-            if (slidesVisible > this.slidesNumber) {
-                if(this.carouselOn){
-                    this.clearSmoothScrolling();
-                }
-            } else {
-                this.initSmoothScrolling(slidesVisible);
+            if (slidesVisible <= this.slidesNumber) {   
+                this.initScrolling(slidesVisible);
             }
         }
 
-        initSmoothScrolling(n) {
+        initScrolling(n) {
             var animationWidth = 0;
             this.slides.each(function () {
                 animationWidth += $(this).outerWidth(false);
@@ -44,24 +47,37 @@
             this.slides.slice(0, n).clone().appendTo(this.container);
 
             // Detect the slider width with appended tail
-            var sliderWidth = 0;
-            this.slides.each(function () {
-                sliderWidth += $(this).outerWidth(false);
-            });
+            var sliderWidth = this.firstSlide.outerWidth(false)*(this.slidesNumber + n);
 
             var sliderHeight = this.firstSlide.outerHeight(false);
 
             // set slider dimensions
             this.container.css({ width: sliderWidth, height: sliderHeight });
 
+            var animationStart = 0;
+            var animationEnd = -animationWidth;
+
             this.animationStyle.innerHTML = `
-            @keyframes smoothscroll { 0% { margin-left: 0px; } 100% { margin-left: -${animationWidth}px; } } 
+            @keyframes smoothscroll { 0% { margin-left: ${animationStart}px; } 100% { margin-left: ${animationEnd}px; } } 
             
-            [data-sysflow-carousel]>div>div:first-of-type {-webkit-animation: smoothscroll ${this.speed} s linear infinite; -moz-animation: smoothscroll ${this.speed}s linear infinite; -ms-animation: smoothscroll ${this.speed}s linear infinite; -o-animation: smoothscroll ${this.speed}s linear infinite; animation: smoothscroll ${this.speed}s linear infinite; }`;
+            [data-sysflow-carousel]>div>div:first-of-type {-webkit-animation: smoothscroll ${this.speed}s linear infinite; -moz-animation: smoothscroll ${this.speed}s linear infinite; -ms-animation: smoothscroll ${this.speed}s linear infinite; -o-animation: smoothscroll ${this.speed}s linear infinite; animation: smoothscroll ${this.speed}s linear infinite; }`;
 
             // Insert styles to html
-            this.appendAnimation();
+            this.startScrolling();
 
+            this.carouselOn = true;
+        }
+
+        clearScrolling() {
+            $(this.cards).css('justify-content','center');
+            this.stopScrolling();
+            var allSlides = $(this.container).children();
+            allSlides.slice(this.slidesNumber, allSlides.length).detach();
+        }
+
+        startScrolling() {
+            $(this.cards).css('justify-content','flex-start');
+            document.head.append(this.animationStyle);
             // restart the animation (e.g. for safari & ie)
             var cl = $(this.cards).attr("class");
             $(this.cards)
@@ -69,18 +85,10 @@
                 .animate({ nothing: null }, 1, function () {
                     $(this).addClass(cl);
                 });
-
-            this.carouselOn = true;
         }
 
-        clearSmoothScrolling() {
+        stopScrolling() {
             this.animationStyle.remove();
-            var allSlides = $(this.container).children();
-            allSlides.slice(this.slidesNumber,allSlides.length).detach();
-        }
-
-        appendAnimation() {
-            document.head.append(this.animationStyle);
         }
     }
 
