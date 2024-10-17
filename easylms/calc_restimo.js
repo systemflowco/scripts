0e15aab7dbfebfdd875a2108656b956eb4f15727
Roza Lewinska <rozalewinska@gmail.com>
2024-10-17 15:46:30 +0200

    Start Month Price

--- easylms/calc_restimo.js
+++ easylms/calc_restimo.js
@@ -4,6 +4,7 @@
             this.items = document.querySelectorAll(".pricing-checkbox-header");
             this.startPrice = 199;
             this.startLabel = "Wdrożenie online";
+            this.startMonthLabel = "Abonament Restimo";
             this.startMonthPrice = 0;
             this.totalPrice = 199;
             this.totalMonthPrice = 0;
@@ -87,6 +88,7 @@
 
         checkSinglePrices() {
             this.addPriceToTable(this.startLabel, this.startPrice);
+            this.addMonthPriceToTable(this.startMonthLabel, this.startMonthPrice);
             this.items.forEach((item, index) => {
                 let chosen =
                     item.querySelector("input[type=radio]")?.checked;
